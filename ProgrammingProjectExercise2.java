31eb852560cba32b73f2c1d2016771fa334674a5
timothycote123 <54383291+timothycote123@users.noreply.github.com>
2019-09-30 11:29:17 -0400

    exercise 2

--- ProgrammingProjectExercise2.java
+++ ProgrammingProjectExercise2.java
@@ -5,17 +5,24 @@ public class ProgrammingProjectExercise2 {
         int xM = 34;
         int yB = 16;
         int yM = 8;
-        double baltimaretomanehattan = distance(xB, xM, yB, yM);
-        System.out.println(baltimaretomanehattan); //Distance from Baltimare to manehattan
+        double baltimareToManehattan = distance(xB, xM, yB, yM);
+        System.out.println(baltimareToManehattan); //Distance from Baltimare to manehattan
         int xL = 6;
         int xN = 22;
         int yL = 19;
         int yN = 7;
-        double lospegasustoneighagrafalls = distance(xL, xN, yL, yN);
-        System.out.println(lospegasustoneighagrafalls); //Distance from Los Pegasus to Neighagra Falls
+        double losPegasusToNeighagraFalls = distance(xL, xN, yL, yN);
+        System.out.println(losPegasusToNeighagraFalls); //Distance from Los Pegasus to Neighagra Falls
+        int xBA = 25;
+        int xP = 16;
+        int yBA = 24;
+        int yP = 14;
+        double badlandsToPonyville = distance(xBA, xP, yBA, yP);
+        System.out.print(badlandsToPonyville); //Distance from Badlands to ponyville
     }
     public static double distance(int x1, int x2, int y1, int y2) {
         return Math.sqrt (Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
+    
 }
         
